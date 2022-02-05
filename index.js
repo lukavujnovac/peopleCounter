17057ffd4c075c8529bcd5613dc76e0722ea3a68
Luka <vujnovacluka89@gmail.com>
2022-02-05 13:16:36 +0100

    increment and decrement buttons made

---
+++ index.js
@@ -0,0 +1,18 @@
+let countEL = document.getElementById("count-el")
+let count = 0
+
+function incremnet() {
+    count += 1
+    countEL.innerText = count
+}
+
+function decrement() {
+    count -= 1
+    countEL.innerText = count
+}
+
+function save() {
+
+}
+
+
