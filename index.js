c645a1cab6618489fcd7f225ae3e3c2239830cd4
Luka <vujnovacluka89@gmail.com>
2022-02-05 22:17:20 +0100

    project completed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let countEL = document.getElementById("count-el")
 let count = 0
+let saveEL = document.getElementById("previousEnteryEL")
 
 function incremnet() {
     count += 1
@@ -8,11 +9,13 @@ function incremnet() {
 
 function decrement() {
     count -= 1
-    countEL.innerText = count
+    countEL.textContent = count
 }
 
 function save() {
+    let countStr = count + " - "
+    saveEL.innerText += countStr
 
+    count = 0
+    countEL.innerText = count
 }
-
-
