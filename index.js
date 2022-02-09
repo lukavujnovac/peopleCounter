f80f0ab534c583659f9d261f417b4b0af7862f3b
Luka <vujnovacluka89@gmail.com>
2022-02-09 17:54:23 +0100

    completed project

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let countEL = document.getElementById("count-el")
 let count = 0
 let saveEL = document.getElementById("previousEnteryEL")
+let errorEL = document.getElementById("error")
 
 function incremnet() {
-    count += 1
-    countEL.innerText = count
+    // count += 1
+    // countEL.innerText = count
+
+    errorEL.textContent = "ERROR"
 }
 
 function decrement() {
@@ -18,4 +21,8 @@ function save() {
 
     count = 0
     countEL.innerText = count
+}
+
+function renderError() {
+    errorEL.textContent = "ERROR"
 }
